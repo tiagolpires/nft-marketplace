273c3c99c8ed0344740d5bba728aeaf3db35ff2b
tiagolpires <tiagolpires@hotmail.com>
2022-03-09 19:58:51 -0300

    feat: init project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="NFTs Marketplace" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
